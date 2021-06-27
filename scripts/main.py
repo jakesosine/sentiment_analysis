a3929c12edccc80ad109a27e769fdc967c3a4bba
Jacob Sosine <51036476+jakesosine@users.noreply.github.com>
2021-06-27 09:08:56 -0700

    resolving issue
    adding some modifications to resolve one line issue

--- scripts/main.py
+++ scripts/main.py
@@ -1,1 +1,30 @@
-import pandas as pd import numpy as npimport """Note: To modify the  python interpretor, import sys and run print(sys.executable) fromthe virtual environment. This will require you to change the path in the settingsto the virtual environment exectuable and restart the KernalPurpose of the script is to """#import nltk#nltk.download('vader_lexicon') #downloading vader lexiconfrom nltk.sentiment.vader import SentimentIntensityAnalyzervader = SentimentIntensityAnalyzer()sample1 = 'I really love working here'sample2 = 'I really hate working here'lst = [sample, sample1,sample2]vader.polarity_scores(sample)for i in lst:    scores = vader.polarity_scores(i)    print(scores['compound'])    
+import pandas as pd 
+import numpy as np
+import 
+
+"""
+Note: To modify the  python interpretor, import sys and run print(sys.executable) from
+the virtual environment. This will require you to change the path in the settings
+to the virtual environment exectuable and restart the Kernal
+Purpose of the script is to 
+"""
+
+
+
+#import nltk
+#nltk.download('vader_lexicon') #downloading vader lexicon
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+vader = SentimentIntensityAnalyzer()
+
+sample1 = 'I really love working here'
+sample2 = 'I really hate working here'
+
+
+lst = [sample, sample1,sample2]
+
+vader.polarity_scores(sample)
+for i in lst:
+    scores = vader.polarity_scores(i)
+    print(scores['compound'])
+    
