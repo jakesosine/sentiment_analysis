52b9b9e89f61dca1768800c5dea9a48db9ff80ba
jakesosine <jacobsosine@gmail.com>
2021-06-27 20:03:03 -0700

    Adding data

--- scripts/main.py
+++ scripts/main.py
@@ -1,18 +1,17 @@
+"""
+Note to self: To add the python interpretor path to the standalone spyder IDE do the following
+1. import sys
+2. run print(sys.executable)
+3. Navigate to Spyder -> Preferences -> Python Interpretor 
+4. Copy result of sys.executable
+5. Paste path in "Use the following python interpretor"
+"""
 import pandas as pd 
 import numpy as np
 #import nltk
 #nltk.download('vader_lexicon') #downloading vader lexicon
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
-
-"""
-Note: To modify the  python interpretor, import sys and run print(sys.executable) from
-the virtual environment. This will require you to change the path in the settings
-to the virtual environment exectuable and restart the Kernal
-Purpose of the script is to 
-"""
-
-
-
+import time
 
 
 
@@ -22,9 +21,9 @@ sample1 = 'I really love working here'
 sample2 = 'I really hate working here'
 
 
-lst = [sample, sample1,sample2]
+lst = [sample1,sample2]
 
-vader.polarity_scores(sample)
+
 for i in lst:
     scores = vader.polarity_scores(i)
     print(scores['compound'])
